34d8bb42de2922719fd5cdccc605fc972ae2bf79
juanpa0180 <102774270+juanpa0180@users.noreply.github.com>
2023-03-17 21:18:10 -0500

    added coupons as replacement for discounts

--- descuentos.js
+++ descuentos.js
@@ -1,21 +1,36 @@
-const inputPrice = document.querySelector('#precio');
-const inputDiscount = document.querySelector('#descuento');
-const resultado = document.querySelector('#resultado');
-const boton = document.querySelector('#calcular');
+const inputPrice = document.querySelector('#price');
+const inputCoupon = document.querySelector('#coupon');
+const btn = document.querySelector('#calcular');
+const pResult = document.querySelector('#result');
 
-boton.addEventListener('click', descuentoAplicado);
+btn.addEventListener('click', calcularPrecioConDescuento);
 
-function descuentoAplicado(){
-    const price = Number(inputPrice.value);
-    const discount = Number(inputDiscount.value);
+const arrayUObjecto = undefined;
 
-    if(!price || !discount){
-        resultado.innerText ="Por favor llena el formulario";
-    } else if(price > 0 && discount < 100 && discount > 0){
-        let descontado = (price * (100 - discount)) / 100; 
+function calcularPrecioConDescuento() {
+  const price = Number(inputPrice.value);
+  const coupon = inputCoupon.value;
 
-        resultado.innerText ="Su nuevo precio con el descuento es " + descontado;
-    } else{
-        resultado.innerText ="Resultado inválido, intenelo de nuevo.";
-    }
+  if (!price || !coupon) {
+    pResult.innerText = 'CHANCLA por favor llena el formulario';
+    return;
+  }
+  
+  let discount;
+
+  switch (coupon) {
+    case 'JuanDC_es_Batman':
+      discount = 30;
+      break;
+    case 'no_le_digas_a_nadie':
+      discount = 25;
+      break;
+    default:
+      pResult.innerText = 'El cupón no es válido';
+      return;
+  }
+  
+  const newPrice = (price * (100 - discount)) / 100;
+
+  pResult.innerText = 'El nuevo precio con descuento es $' + newPrice;
 }
