2b11f94d840b4b4cd23b46f0adf6b1d04559f733
juanpa0180 <102774270+juanpa0180@users.noreply.github.com>
2023-03-21 16:37:10 -0500

    finished the discount (switch, objects and arrays), also created estadistica files

--- descuentos.js
+++ descuentos.js
@@ -5,7 +5,27 @@ const pResult = document.querySelector('#result');
 
 btn.addEventListener('click', calcularPrecioConDescuento);
 
-const arrayUObjecto = undefined;
+//const arrayUObjecto = undefined;
+
+/*const couponsObj = {
+  'JuanDC_es_Batman' : 30,
+  '123' : 25,
+  'no_le_digas_a_nadie' : 15
+}*/
+
+const couponsList = [];
+couponsList.push({
+  name: 'JuanDC_es_Batman',
+  discount: 30,
+})
+couponsList.push({
+  name: 'Pero_es_un_secreto',
+  discount: 25,
+})
+couponsList.push({
+  name: 'no_le_digas_a_nadie',
+  discount: 20,
+})
 
 function calcularPrecioConDescuento() {
   const price = Number(inputPrice.value);
@@ -18,7 +38,30 @@ function calcularPrecioConDescuento() {
   
   let discount;
 
-  switch (coupon) {
+  function isCouponInArray(couponElement){
+    return couponElement.name == coupon;
+  }
+
+  const couponInArray = couponsList.find(isCouponInArray);
+  //const couponInArray = couponsList.filter(isCouponInArray);
+
+  if(couponInArray){
+  //if(couponInArray.length>0){
+    discount = couponInArray.discount;
+  //  discount = couponInArray[0].discount;
+  } else {
+    pResult.innerText = 'El cupón no es válido';
+    return;
+  }
+
+  /*if(couponsObj[coupon]){
+    discount = couponsObj[coupon]
+  } else {
+    pResult.innerText = 'El cupón no es válido';
+    return;
+  }*/
+
+  /*switch (coupon) {
     case 'JuanDC_es_Batman':
       discount = 30;
       break;
@@ -28,7 +71,7 @@ function calcularPrecioConDescuento() {
     default:
       pResult.innerText = 'El cupón no es válido';
       return;
-  }
+  }*/
   
   const newPrice = (price * (100 - discount)) / 100;
 
